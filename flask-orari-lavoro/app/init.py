5d31551110ee466b7540bf13282222392182cd92
Oxide <jascio1996@hotmail.com>
2023-11-09 16:54:49 +0100

    Update init.py

--- flask-orari-lavoro/app/init.py
+++ flask-orari-lavoro/app/init.py
@@ -3,6 +3,6 @@ import sqlite3
 
 connection = sqlite3.connect('/share/flask-orari-lavoro/database.db')
 connection.row_factory = sqlite3.Row
-connection.execute('CREATE TABLE IF NOT EXISTS "PRESENZE" (	"id"	integer,	"giorno"	varchar2(20),	"oranetr"	varchar2(20),	"orausci"	varchar2(20),	"pausa"	varchar2(1) DEFAULT N,	PRIMARY KEY("id" AUTOINCREMENT))'
+connection.execute('CREATE TABLE IF NOT EXISTS "PRESENZE" (	"id"	integer,	"giorno"	varchar2(20),	"oranetr"	varchar2(20),	"orausci"	varchar2(20),	"pausa"	varchar2(1) DEFAULT N,	PRIMARY KEY("id" AUTOINCREMENT))')
 connection.commit()
 connection.close()
